40526a832bc426cb4810bacf8cfcdf52e0d8ff70
Dots <dotvnl@gmail.com>
2024-05-29 18:47:02 +0300

    refactoring, bump version

--- SimpleGunMenuPlugin.cs
+++ SimpleGunMenuPlugin.cs
@@ -8,8 +8,8 @@ namespace SimpleGunMenuPlugin;
 public partial class SimpleGunMenuPlugin : BasePlugin
 {
     public override string ModuleName => "GunMenuPlugin";
-    public override string ModuleVersion => "0.0.4";
-    public override string ModuleAuthor => "Constummer";
+    public override string ModuleVersion => "0.0.4-fix";
+    public override string ModuleAuthor => "Constummer (Dots fix)";
     public override string ModuleDescription => "Gun Menu Plugin";
 
     public override void Load(bool hotReload)
@@ -17,7 +17,7 @@ public partial class SimpleGunMenuPlugin : BasePlugin
     }
 
     [ConsoleCommand("guns")]
-    public void Guns(CCSPlayerController? player, CommandInfo info)
+    public static void Guns(CCSPlayerController? player, CommandInfo info)
     {
         if (ValidatePlayer(player) == false)
         {
@@ -26,12 +26,12 @@ public partial class SimpleGunMenuPlugin : BasePlugin
 
         var gunMenu = new ChatMenu("Gun Menu");
         MenuHelper.GetGuns(gunMenu);
-        ChatMenus.OpenMenu(player, gunMenu);
+        MenuManager.OpenChatMenu(player, gunMenu);
     }
 
     [ConsoleCommand("pistols")]
     [ConsoleCommand("secondary")]
-    public void Pistols(CCSPlayerController? player, CommandInfo info)
+    public static void Pistols(CCSPlayerController? player, CommandInfo info)
     {
         if (ValidatePlayer(player) == false)
         {
@@ -40,12 +40,12 @@ public partial class SimpleGunMenuPlugin : BasePlugin
 
         var gunMenu = new ChatMenu($"{info.GetCommandString.Split(" ")[0]} Menu");
         MenuHelper.GetGuns(gunMenu, WeaponType.Secondary);
-        ChatMenus.OpenMenu(player, gunMenu);
+        MenuManager.OpenChatMenu(player, gunMenu);
     }
 
     [ConsoleCommand("rifles")]
     [ConsoleCommand("primary")]
-    public void Rifles(CCSPlayerController? player, CommandInfo info)
+    public static void Rifles(CCSPlayerController? player, CommandInfo info)
     {
         if (ValidatePlayer(player) == false)
         {
@@ -54,7 +54,7 @@ public partial class SimpleGunMenuPlugin : BasePlugin
 
         var gunMenu = new ChatMenu($"{info.GetCommandString.Split(" ")[0]} Menu");
         MenuHelper.GetGuns(gunMenu, WeaponType.Primary);
-        ChatMenus.OpenMenu(player, gunMenu);
+        MenuManager.OpenChatMenu(player, gunMenu);
     }
 
     private static bool ValidatePlayer(CCSPlayerController? player)
