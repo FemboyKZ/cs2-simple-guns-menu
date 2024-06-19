46d3da584be989f1d83ca26ab5eee95305e00eaf
Dots <dotvnl@gmail.com>
2024-06-19 22:56:41 +0300

    fix plugin load by using AddCommand

--- SimpleGunMenuPlugin.cs
+++ SimpleGunMenuPlugin.cs
@@ -1,75 +1,91 @@
 ﻿using CounterStrikeSharp.API.Core;
-using CounterStrikeSharp.API.Core.Attributes.Registration;
-using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Core.Attributes;
 using CounterStrikeSharp.API.Modules.Menu;
 using CounterStrikeSharp.API.Modules.Utils;
 
 namespace SimpleGunMenuPlugin;
 
+[MinimumApiVersion(240)]
 public partial class SimpleGunMenuPlugin : BasePlugin
 {
     public override string ModuleName => "GunMenuPlugin";
-    public override string ModuleVersion => "0.0.5-fix";
+    public override string ModuleVersion => "0.0.6-fix";
     public override string ModuleAuthor => "Constummer (Dots fix)";
     public override string ModuleDescription => "Gun Menu Plugin";
 
     public override void Load(bool hotReload)
     {
-    }
-
-    [ConsoleCommand("gun")]
-    [ConsoleCommand("guns")]
-    [ConsoleCommand("weapon")]
-    [ConsoleCommand("weapons")]
-    public static void Guns(CCSPlayerController? player, CommandInfo info)
-    {
-        if (ValidatePlayer(player) == false)
+        AddCommand("css_guns", "Opens the gun menu", (player, commandInfo) =>
+        {
+        if (ValidatePlayer(player) == false || player == null)
         {
             return;
         }
 
         var gunMenu = new ChatMenu("Gun Menu");
         MenuHelper.GetGuns(gunMenu);
-        if (player != null)
-        {
-            MenuManager.OpenChatMenu(player, gunMenu);
-        }
-    }
+        MenuManager.OpenChatMenu(player, gunMenu);
+        });
 
-    [ConsoleCommand("pistol")]
-    [ConsoleCommand("pistols")]
-    [ConsoleCommand("secondary")]
-    public static void Pistols(CCSPlayerController? player, CommandInfo info)
-    {
-        if (ValidatePlayer(player) == false)
+        AddCommand("css_weapons", "Opens the gun menu", (player, commandInfo) =>
+        {
+        if (ValidatePlayer(player) == false || player == null)
         {
             return;
         }
 
-        var gunMenu = new ChatMenu($"{info.GetCommandString.Split(" ")[0]} Menu");
+        var gunMenu = new ChatMenu("Gun Menu");
+        MenuHelper.GetGuns(gunMenu);
+        MenuManager.OpenChatMenu(player, gunMenu);
+        });
+
+        AddCommand("css_pistols", "Opens the pistol menu", (player, commandInfo) =>
+        {
+        if (ValidatePlayer(player) == false || player == null)
+        {
+            return;
+        }
+
+        var gunMenu = new ChatMenu($"{commandInfo.GetCommandString.Split(" ")[0]} Menu");
         MenuHelper.GetGuns(gunMenu, WeaponType.Secondary);
-        if (player != null)
-        {
-            MenuManager.OpenChatMenu(player, gunMenu);
-        }
-    }
+        MenuManager.OpenChatMenu(player, gunMenu);
+        });
 
-    [ConsoleCommand("rifle")]
-    [ConsoleCommand("rifles")]
-    [ConsoleCommand("primary")]
-    public static void Rifles(CCSPlayerController? player, CommandInfo info)
-    {
-        if (ValidatePlayer(player) == false)
+        AddCommand("css_secondary", "Opens the pistol menu", (player, commandInfo) =>
+        {
+        if (ValidatePlayer(player) == false || player == null)
         {
             return;
         }
 
-        var gunMenu = new ChatMenu($"{info.GetCommandString.Split(" ")[0]} Menu");
-        MenuHelper.GetGuns(gunMenu, WeaponType.Primary);
-        if (player != null)
+        var gunMenu = new ChatMenu($"{commandInfo.GetCommandString.Split(" ")[0]} Menu");
+        MenuHelper.GetGuns(gunMenu, WeaponType.Secondary);
+        MenuManager.OpenChatMenu(player, gunMenu);
+        });
+
+        AddCommand("css_rifles", "Opens the rifle menu", (player, commandInfo) =>
         {
-            MenuManager.OpenChatMenu(player, gunMenu);
+        if (ValidatePlayer(player) == false || player == null)
+        {
+            return;
         }
+
+        var gunMenu = new ChatMenu($"{commandInfo.GetCommandString.Split(" ")[0]} Menu");
+        MenuHelper.GetGuns(gunMenu, WeaponType.Primary);
+        MenuManager.OpenChatMenu(player, gunMenu);
+        });
+
+        AddCommand("css_pirmary", "Opens the rifle menu", (player, commandInfo) =>
+        {
+        if (ValidatePlayer(player) == false || player == null)
+        {
+            return;
+        }
+
+        var gunMenu = new ChatMenu($"{commandInfo.GetCommandString.Split(" ")[0]} Menu");
+        MenuHelper.GetGuns(gunMenu, WeaponType.Primary);
+        MenuManager.OpenChatMenu(player, gunMenu);
+        });
     }
 
     private static bool ValidatePlayer(CCSPlayerController? player)
